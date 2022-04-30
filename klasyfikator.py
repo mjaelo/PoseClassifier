1708643a14f3f308f4bf4e0d9a210da106281b75
mjaelo <44395343+mjaelo@users.noreply.github.com>
2022-04-30 17:25:20 +0200

    added ml + better traindata
    video_editor.py gives better train data, by averaging prev results
    added ml in pose classification. right now its only tested on train data.

--- klasyfikator.py
+++ klasyfikator.py
@@ -1,7 +1,58 @@
 import xlsxwriter
 import xlrd
 
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+
 expected = []
+
+
+# stand 375
+# sit 281
+# lay 202
+# bow 179
+# undefined 0
+
+def predict_pose(data):
+    global expected
+    # x data, y labels
+    # train = expected. X data. Y labels
+    X_train = data
+    Y_train = expected[:1028]
+    X_validation = data
+    # train model
+    vectorizer = TfidfVectorizer()
+    regressor = LogisticRegression()
+    pipeline = Pipeline([('vectorizer', vectorizer), ('regressor', regressor)])
+    pipeline.fit(X_train, Y_train)
+    # predict
+    result = pipeline.predict(X_validation)  # value in '0','1', etc
+    return result
+
+
+def get_BP_data(loc):
+    # read from xls
+    global expected
+    wb = xlrd.open_workbook(loc)
+    sheet = wb.sheet_by_index(0)
+    all_frames = []
+    for i in range(1028):  # row
+        i += 1
+        row = []
+        for j in range(32):  # column
+            cell = sheet.cell_value(i, j)
+            cell = cell[1:len(cell) - 1]
+            cell = cell.split(',')
+            cell = [int(cell[0]), int(cell[1])]
+            row.append(cell)
+        all_frames.append(str(row))
+    return all_frames
 
 
 def read_excel():
@@ -30,25 +81,128 @@ def write_to_excel():
     book.close()
 
 
-# 0=?, 1=stand, 2=sit, 3=lay, 4=bow
-def interpret_pose(nose, hips,  knees, feet):
-    # standing = nose>hips>knees>feet
-    if nose[1] < hips[1] < knees[1] < feet[1]:
+def measure_distance_XY(point1, point2):
+    disX = point2[0] - point1[0]
+    disY = point2[1] - point1[1]
+    if disX > disY:
+        return 0
+    else:
         return 1
+
+
+# 0=?, 1=stand, 2=sit, 3=lay, 4=bow
+# 57%
+def interpret_pose(nose, hips, knees, feet):
+    # measure distance between joints
+    # 0 = x distance longer, 1 y distance longer
+    n_to_h = measure_distance_XY(nose, hips)
+    h_to_k = measure_distance_XY(hips, knees)
+    k_to_f = measure_distance_XY(knees, feet)
+
+    # standing = nose>hips>knees>feet
+    if n_to_h == 1 and h_to_k == 1 and k_to_f == 1 and (nose[1] < hips[1] < knees[1] < feet[1]) and (
+            feet[1] - nose[1] > 700) and int(hips[0] / 100) == int(nose[0] / 100):
+        return 1
+    # bowing = nose<hips>knees>feet
+    elif k_to_f == 1 and (n_to_h == 0 or int(hips[1] / 100) == int(nose[1] / 100)):
+        return 4
     # sitting = nose>hips=knees>feet
-    elif nose[1] < hips[1] == knees[1] < feet[1]:
+    elif n_to_h == 1 and int(hips[1] / 100) == int(feet[1] / 100):
         return 2
     # laying = nose=hips=knees
-    elif nose[1] == hips[1] == knees[1]:
+    elif (n_to_h == 0 and h_to_k == 0 and k_to_f == 0) or (n_to_h == 1 and h_to_k == 1 and k_to_f == 1):
+        return 3
+    else:
+        return 0
+
+
+#  10%
+def interpret_pose_old(nose, hips, knees, feet):
+    nose = [int(nose[0] / 10), int(nose[1] / 10)]
+    hips = [int(hips[0] / 10), int(hips[1] / 10)]
+    knees = [int(knees[0] / 10), int(knees[1] / 10)]
+    feet = [int(feet[0] / 10), int(feet[1] / 10)]
+
+    # standing = nose>hips>knees>feet
+    if (nose[1] < hips[1] < knees[1] < feet[1]) and (int(nose[0] / 10) == int(hips[0] / 10) == int(knees[0] / 10)):
+        return 1
+    # sitting = nose>hips=knees>feet
+    elif (nose[1] < hips[1] and int(hips[1] / 10) == int(knees[1] / 10) and knees[1] < feet[1]):
+        return 2
+    # laying = nose=hips=knees
+    elif (int(nose[1] / 10) == int(hips[1] / 10) == int(knees[1] / 10)) and (nose[0] < hips[0] < knees[0] < feet[0]):
         return 3
     # bowing = nose<hips>knees>feet
-    elif nose[1] > hips[1] < knees[1] < feet[1]:
+    elif (nose[1] > hips[1] < knees[1] < feet[1]):
         return 4
     else:
         return 0
 
 
-def get_pose(loc):
+def result_statistics(poses):
+    global expected
+    point_nr = 0
+    correct_poses = 0
+    frame = 0
+    startframe = frame
+
+    nr_stand = 0
+    nr_sit = 0
+    nr_lay = 0
+    nr_bow = 0
+    nr_und = 0
+    cr_stand = 0
+    cr_sit = 0
+    cr_lay = 0
+    cr_bow = 0
+    cr_und = 0
+
+    # statistics of correctness
+    for i in range(1028):  # row
+        if len(poses) > 1 and poses[i] != poses[i - 1]:
+            correct = 0
+            for k in range(frame - startframe):
+                if str(poses[k + startframe]) == expected[k + startframe]:
+                    correct += 1
+
+            if poses[i - 1] == '1':
+                print("standing: (", startframe, frame, ") correct:", correct, '/', frame - startframe)
+                cr_stand += correct
+                nr_stand += frame - startframe
+            elif poses[i - 1] == '2':
+                print("sitting: (", startframe, frame, ") correct:", correct, '/', frame - startframe)
+                cr_sit += correct
+                nr_sit += frame - startframe
+            elif poses[i - 1] == '3':
+                print("laying: (", startframe, frame, ") correct:", correct, '/', frame - startframe)
+                cr_lay += correct
+                nr_lay += frame - startframe
+            elif poses[i - 1] == '4':
+                print("bowing: (", startframe, frame, ") correct:", correct, '/', frame - startframe)
+                cr_bow += correct
+                nr_bow += frame - startframe
+            else:
+                print("undefined: (", startframe, frame, ") correct:", correct, '/', frame - startframe)
+                cr_und += correct
+                nr_und += frame - startframe
+            startframe = frame
+        frame += 1
+
+        # verify_poses
+        if (poses[i] == expected[i]):
+            correct_poses += 1
+
+    print("\nTotal correctness:")
+    print("Standing: ", cr_stand, nr_stand)
+    print("Sitting: ", cr_sit, nr_sit)
+    print("Laying: ", cr_lay, nr_lay)
+    print("Bowing: ", cr_bow, nr_bow)
+    print("Undefined: ", cr_und, nr_und)
+
+    return point_nr, correct_poses, i
+
+
+def check_pose(loc):
     global expected
 
     wb = xlrd.open_workbook(loc)
@@ -90,19 +244,19 @@ def get_pose(loc):
                 if str(poses[k + startframe]) == expected[k + startframe]:
                     correct += 1
 
-            if pose == 1:
+            if poses[-2] == 1:
                 print("standing: (", startframe, frame, ") correct:", correct, '/', frame - startframe)
                 cr_stand += correct
                 nr_stand += frame - startframe
-            elif pose == 2:
+            elif poses[-2] == 2:
                 print("sitting: (", startframe, frame, ") correct:", correct, '/', frame - startframe)
                 cr_sit += correct
                 nr_sit += frame - startframe
-            elif pose == 3:
+            elif poses[-2] == 3:
                 print("laying: (", startframe, frame, ") correct:", correct, '/', frame - startframe)
                 cr_lay += correct
                 nr_lay += frame - startframe
-            elif pose == 4:
+            elif poses[-2] == 4:
                 print("bowing: (", startframe, frame, ") correct:", correct, '/', frame - startframe)
                 cr_bow += correct
                 nr_bow += frame - startframe
@@ -135,9 +289,13 @@ def import_files():
         expected.append(result)
 
     # BlazePose
-    point_nr, correct_poses, poses_total = get_pose("BP_f10.xls")
+    # point_nr, correct_poses, poses_total = check_pose("BP_f10.xls")
+    BP_data = get_BP_data("BP_f10.xls")
+    result = predict_pose(BP_data)
+    point_nr, correct_poses, poses_total = result_statistics(result)
     print("\npoints in f10 BP: ", point_nr)
-    print("correct poses in f10 BP: ", correct_poses, '/', poses_total)
+    print("correct poses in f10 BP: ", correct_poses, '/', poses_total, ",", int(correct_poses / poses_total * 100),
+          '%')
     # write to excel
 
     # OpenPose
--- video_editor.py
+++ video_editor.py
@@ -1,17 +1,67 @@
+from collections import Counter
+
 import cv2
 import numpy as np
 
-movie="mat_color"
-cap = cv2.VideoCapture(movie+'.mp4')
+movie = "mat_color"
+cap = cv2.VideoCapture(movie + '.mp4')
+
+
 # mat_color  nr of frames:  1037
 
 
 # fourcc = cv2.VideoWriter_fourcc(*'XVID')
 # out = cv2.VideoWriter('output.mp4', -1, 20.0, (640, 480))
 
+
+def append_txt(l1):
+    file1 = open(movie + "_all.txt", "r+")
+    l1 = file1.read() + l1
+
+    file1 = open(movie + "_all.txt", "w")
+    file1.writelines(l1 + "\n")
+    file1.close()
+    cap.release()
+    cv2.destroyAllWindows()
+
+    file1 = open(movie + "_all.txt", "r+")
+    line = "temp"
+    lines = []
+    while line != "":
+        line = file1.readline()
+        if line != "":
+            lines.append(line)
+            print(line)
+    return count_avg(lines)
+
+
+def count_avg(lines):
+    avg_line = ''
+    for i in range(len(lines[0]) - 1):
+        frame_poses = []
+        for li in lines:
+            frame_poses.append(li[i])
+        occurence_count = Counter(frame_poses)
+        most_common = occurence_count.most_common(1)[0][0]
+        avg_line += most_common
+    print("avg:", avg_line)
+    return avg_line
+
+
 def read_txt():
-    file1 = open("maunal_results.txt", "r+")
-    print(file1.read())
+    file1 = open(movie + "_expected.txt", "r+")
+    list = file1.read()
+    nr = list.count('1')
+    print("stand", nr)
+    nr = list.count('2')
+    print("sit", nr)
+    nr = list.count('3')
+    print("lay", nr)
+    nr = list.count('4')
+    print("bow", nr)
+    nr = list.count('0')
+    print("undefined", nr)
+
 
 def play_check():
     nr = 0
@@ -28,28 +78,28 @@ def play_check():
     nr += 1
 
     go = 0
-    list=[]
+    list = ''
     try:
         # 0=?, 1=stand, 2=sit, 3=lay, 4=bow
         while key != ord('q'):
             if key == ord(' '):
-                go=1
-                list.append('0')
+                go = 1
+                list += '0'
             elif key == ord('1'):
-                go=1
-                list.append('1')
+                go = 1
+                list += '1'
             elif key == ord('2'):
-                go=1
-                list.append('2')
+                go = 1
+                list += '2'
             elif key == ord('3'):
-                go=1
-                list.append('3')
+                go = 1
+                list += '3'
             elif key == ord('4'):
-                go=1
-                list.append('4')
+                go = 1
+                list += '4'
             else:
-                go=0
-            if go==1:
+                go = 0
+            if go == 1:
                 ret, img = cap.read()
                 # resize image
                 width = int(img.shape[1] / 2)
@@ -63,72 +113,17 @@ def play_check():
             key = cv2.waitKey(30)
     except:
         print("nr of frames: ", nr)
-        file1 = open(movie+"_expected.txt", "w")
+
+        list = append_txt(list)
+
+        file1 = open(movie + "_expected.txt", "w")
         file1.writelines(list)
         file1.close()
-
-        print("saved frames:",len(list))
+        print("saved frames:", len(list))
         cap.release()
         cv2.destroyAllWindows()
-
-
-def play_frame():
-    nr = 0
-    key = ord('a')
-    ret, img = cap.read()
-
-    # resize image
-    width = int(img.shape[1] / 2)
-    height = int(img.shape[0] / 2)
-    dim = (width, height)
-    img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
-
-    cv2.putText(img, str(nr), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 8, 8), 3)
-    nr += 1
-
-    go=0
-    while key != ord('q'):
-        if key == ord(' '):
-            ret, img = cap.read()
-
-            # resize image
-            width = int(img.shape[1]/2)
-            height = int(img.shape[0]/2)
-            dim = (width, height)
-            img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
-
-            cv2.putText(img, str(nr), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 8, 8), 3)
-            nr += 1
-        cv2.imshow('frame', img)
-        key = cv2.waitKey(30)
-    print("nr of frames: ", nr)
-    cap.release()
-    cv2.destroyAllWindows()
-
-
-def play():
-    nr = 0
-
-    while cap.isOpened():
-        success, img = cap.read()
-        if success:
-            # cv2.rectangle(img, (384, 500), (1000, 128), (0, 0, 0), -1)
-            # for i in range(len(img)):
-            # for j in range(len(img[0])):
-            # p1 = img[i][j]
-            # img[i][j] = [p1[0] / 2, p1[1] / 2, p1[2] / 2]
-            # out.write(img)
-            nr += 1
-            cv2.imshow("Image", img)
-            if cv2.waitKey(25) & 0xFF == ord('q'):
-                break
-        else:
-            break
-    print("nr of frames: ", nr)
-    cap.release()
-    # out.release()
-    cv2.destroyAllWindows()
+        read_txt()
 
 if __name__ == '__main__':
-    #play_check()
-    read_txt()
+    play_check()
+    # read_txt()
